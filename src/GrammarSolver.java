076cc5f89c58bdc16a6211c9fe73c926424a2004
javadev115 <coxscott64@gmail.com>
2021-11-02 17:39:45 -0700

    Add comments to GrammarSolver

--- src/GrammarSolver.java
+++ src/GrammarSolver.java
@@ -1,3 +1,13 @@
+/**
+ * Name: GrammarSolver.java
+ * TA: Kashish Aggarval
+ * 
+ * Takes a list of grammar rules in Bauckus-Naur form and generates random
+ * sentences that follow the given Bauckus-Naur grammar rules.
+ * 
+ * @author Victor Du
+ */
+
 import java.util.*;
 
 public class GrammarSolver
@@ -5,6 +15,19 @@ public class GrammarSolver
     private SortedMap<String, String[]> ruleMap;
     private Random random;
 
+    /**
+     * Initializes a GrammarSolver instance given a List<String> of Bauckus-Naur
+     * grammar rules for the GrammarSolver instance to follow, one rule per
+     * entry.
+     * 
+     * @param rules,
+     *            a List<String> of Bauckus-Naur grammar rules for the
+     *            GrammarSolver instance to follow.
+     * 
+     * @throws IllegalArgumentException
+     *             if rules is empty or contains more than one BNF grammar rule
+     *             with the same non-terminal.
+     */
     public GrammarSolver(List<String> rules)
     {
         if (rules.isEmpty())
@@ -23,7 +46,8 @@ public class GrammarSolver
             if (ruleMap.containsKey(nonTerminal))
             {
                 throw new IllegalArgumentException(
-                    String.format("More than one entry for nonterminal %s in rules list.", nonTerminal));
+                    String.format("More than one entry for nonterminal %s in rules list.", 
+                        nonTerminal));
             }
 
             String[] optionsArr = parts[1].split("\\|");
@@ -39,16 +63,38 @@ public class GrammarSolver
 
     }
 
+    /**
+     * @param symbol,
+     *            the symbol to check whether the GrammarSolver contains a
+     *            non-terminal by that name in its grammar rules.
+     * 
+     * @return whether the GrammarSolver instance contains a grammar rule by the
+     *         given non-terminal, symbol.
+     */
     public boolean grammarContains(String symbol)
     {
         return ruleMap.containsKey(symbol);
     }
 
+    /**
+     * @return a String representation of all non-terminal symbols in the
+     *         GrammarSolver instance's grammar rules in the form of a list
+     *         that's square bracket-enclosed and comma-separated.
+     */
     public String getSymbols()
     {
         return ruleMap.keySet().toString();
     }
 
+    /**
+     * Internal helper method to generate a sentence given the non-terminal
+     * symbol to start the sentence from.
+     * 
+     * @param symbol,
+     *            the non-terminal symbol to generate the sentence from.
+     * 
+     * @return a sentence generated from the non-terminal symbol String, symbol.
+     */
     private String generate(String symbol)
     {
         String s = "";
@@ -61,8 +107,10 @@ public class GrammarSolver
         for (int i = 0; i < tokens.length; i++)
         {
             String token = tokens[i];
-            // resolve each token if non-terminal
+
+            // resolve each token recursively if non-terminal
             s += ruleMap.containsKey(token) ? generate(token) : token;
+
             if (i != tokens.length - 1)
             {
                 s += " ";
@@ -72,12 +120,31 @@ public class GrammarSolver
         return s;
     }
 
+    /**
+     * Procedurally generates a String[] of size times, of sentences compliant
+     * with the grammar rules of the GrammarSolver instance from a given
+     * non-terminal symbol.
+     * 
+     * @param symbol,
+     *            the non-terminal symbol to generate the sentences from.
+     * 
+     * @param times,
+     *            the number of sentences to generate. Must be greater than
+     *            zero.
+     * 
+     * @throws IllegalArgumentException
+     *             if times is negative or if symbol is not a non-terminal.
+     * 
+     * @return a String[] of sentences compliant with the grammar rules of the
+     *         GrammarSolver instance.
+     */
     public String[] generate(String symbol, int times)
     {
         if (times < 0 || !ruleMap.containsKey(symbol))
         {
             throw new IllegalArgumentException(
-                times < 0 ? "times cannot be negative." : "symbol must be a non-terminal.");
+                times < 0 ? "times cannot be negative." : 
+                    "symbol must be a non-terminal.");
         }
 
         String[] result = new String[times];
