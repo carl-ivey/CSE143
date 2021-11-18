34b69fdff0513b7bf8ec6fec1b2493df186b29e7
javadev115 <coxscott64@gmail.com>
2021-11-17 19:34:07 -0800

    Add comments to AnagramSolver

--- src/AnagramSolver.java
+++ src/AnagramSolver.java
@@ -1,3 +1,14 @@
+/**
+ * Name: AnagramSolver.java
+ * TA: Kashish Aggarval
+ * 
+ * Given a list of words and a word to test, prints all of the 
+ * combinations of anagrams of the word to test from the given 
+ * list of words.
+ * 
+ * @author Victor Du
+ */
+
 import java.util.*;
 
 public class AnagramSolver
@@ -5,6 +16,12 @@ public class AnagramSolver
     private Map<String, LetterInventory> invMap;
     private List<String> wordList;
 
+    /**
+     * Initializes an AnagramSolver instance given a List<String> of
+     * words to search for anagrams in.
+     * 
+     * @param dictionary, the List<String> of words to search for anagrams in.
+     */
     public AnagramSolver(List<String> dictionary)
     {
         invMap = new HashMap<>();
@@ -16,6 +33,18 @@ public class AnagramSolver
         }
     }
     
+    /**
+     * Internal helper method to prune a given List<String> of words for
+     * words that for sure cannot be anagrams of a given String, by
+     * containing extraneous characters, and return a List<String> of the
+     * contents of the given List<String> with the words containing extraneous
+     * characters pruned.
+     * 
+     * @param text, the String to prune the word list against.
+     * @param wordList, the List<String> of words to prune.
+     * @return a List<String> of words containing the contents of wordList,
+     *          with all impossible Strings pruned.
+     */
     private List<String> getPrunedWords(String text, List<String> wordList)
     {
         LetterInventory curInv = new LetterInventory(text);
@@ -34,6 +63,18 @@ public class AnagramSolver
         return pruned;
     }    
 
+    /**
+     * Internal helper method to print all combinations of words in the given
+     * List<String> choices that are anagrams of any word with the contents
+     * of a given LetterInventory instance.
+     * 
+     * @param choices, the List<String> to search anagram combinations inside.
+     * @param remaining, the LetterInventory of letter counts of the search word's contents.
+     * @param res, a Stack<String>, initially blank,
+     *             used to temporarily track the current working anagram combination
+     * @param max, the number of combinations to print before ending the search.
+     * @param limOn, whether to limit the search to max number of results.
+     */
     private void computeAnagrams(List<String> choices, LetterInventory remaining, 
                                     Stack<String> res, int max, boolean limOn)
     {
@@ -57,15 +98,25 @@ public class AnagramSolver
         }
     }
     
+    /**
+     * Prints all combinations of words in the AnagramSolver that are anagrams of
+     * the given String, text.
+     * 
+     * @param text, the String to print all combinations of anagrams of.
+     * @param max, the number of combinations to print before ending the search,
+     *              or zero if all combinations should be printed without limit.
+     * @throws IllegalArgumentException, if max is less than zero.
+     */
     public void print(String text, int max)
     {
         if (max < 0)
         {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(
+                "Max cannot be less than zero.");
         }
         
         List<String> prunedList = getPrunedWords(text, wordList);
-        computeAnagrams(prunedList, new LetterInventory(text), 
+        computeAnagrams(prunedList, new LetterInventory(text),
             new Stack<String>(), max, max != 0);
     }
 }
